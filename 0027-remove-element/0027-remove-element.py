5b2457478451a0b4d8bc226a34382c862c08a32e
GSSHEMANTH <152717976+GSSHEMANTH@users.noreply.github.com>
2026-07-31 10:40:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.56%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        for i in nums[:]:
+            if i==val:
+                nums.remove(i)
+        return len(nums)
+        
+        
